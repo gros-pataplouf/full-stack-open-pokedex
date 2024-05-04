a4f6cfd6faf5e12071a4d9ee5ad9a9f66f007657
gros-pataplouf <sceres@pm.me>
2024-05-04 15:04:25 +0200

    modify pipeline to include render deploy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/*', function(req,res) {
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
